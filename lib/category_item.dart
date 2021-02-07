888372ac98b80cad514eeed5a4a757b660ca57bc
Akshay Sanghai <akshay.sanghai@booking.com>
2021-02-07 15:21:10 +0530

    define static const for routename in category meals screen

--- lib/category_item.dart
+++ lib/category_item.dart
@@ -10,10 +10,13 @@ class CategoryItem extends StatelessWidget {
   CategoryItem(this.id, this.title, this.color);
 
   void selectCategory(BuildContext context) {
-    Navigator.of(context).pushNamed('/category-meals', arguments: {
-      'id': id,
-      'title': title,
-    });
+    Navigator.of(context).pushNamed(
+      CategoryMealsScreen.routeName,
+      arguments: {
+        'id': id,
+        'title': title,
+      },
+    );
   }
 
   @override
