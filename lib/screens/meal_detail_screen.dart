e8077e7bc6f4fabb0054a3ab2ae7483e0eb04f9b
Akshay Sanghai <akshay.sanghai@booking.com>
2021-02-12 19:32:10 +0530

    passing data back with navigator pop

--- lib/screens/meal_detail_screen.dart
+++ lib/screens/meal_detail_screen.dart
@@ -96,6 +96,10 @@ class MealDetailScreen extends StatelessWidget {
           ],
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.delete),
+        onPressed: () => Navigator.of(context).pop(mealId),
+      ),
     );
   }
 }
