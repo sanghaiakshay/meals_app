d84c92b1592c8599f7a77664ac5de1d2472d7d98
Akshay Sanghai <akshay.sanghai@booking.com>
2021-02-12 16:25:06 +0530

    add drawer widget and use pushreplacement named and a dummy filters screen

---
+++ lib/screens/filters_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/widgets/main_drawer.dart';
+
+class FiltersScreen extends StatelessWidget {
+  static const routeName = '/filters';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: MainDrawer(),
+      appBar: AppBar(
+        title: Text('Your Filters'),
+      ),
+      body: Center(
+        child: Text("Filters"),
+      ),
+    );
+  }
+}
--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import './categories_screen.dart';
 import './favorites_screen.dart';
+import '../widgets/main_drawer.dart';
 
 class TabsScreen extends StatefulWidget {
   @override
@@ -33,6 +34,7 @@ class _TabsScreenState extends State<TabsScreen> {
       appBar: AppBar(
         title: Text(_pages[selectedPageIndex]['title']),
       ),
+      drawer: MainDrawer(),
       body: _pages[selectedPageIndex]['page'],
       bottomNavigationBar: BottomNavigationBar(
         onTap: _selectPage,
